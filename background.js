12d91f9cc395457058b39a4f54a9038e62c68b84
Vamsi Charan Adari <adarivamsi@gmail.com>
2023-03-11 20:54:24 -0600

    Modified manifest.json

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
 chrome.contextMenus.create({
   title: "Currency Conversion Menu Item",
-  contexts: ["selection"],
-  onclick: function (info, tab) {
+  id: "currencyConversionId",
+  contexts: ["page"],
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId == "currencyConversionId") {
     alert("Clicked on currency conversion menu item!");
-  },
+  }
 });
